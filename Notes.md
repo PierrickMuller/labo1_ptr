3688e2b37228698c7d994989013d491f15a5180a
Pierrick Muller <pierrick.muller@heig-vd.ch>
2019-10-01 10:18:22 +0200

    Near end

--- Notes.md
+++ Notes.md
@@ -1,8 +1,48 @@
 # Réponses questions
 
+### 1ères mesures
+*Le résultat est le nombre d’opérations par seconde (quelles operations ?)*
+- oui
+
+*Essayez différentes optimisations à l’aide des arguments -O2 et -O3 de gcc et commentez les
+résultats.*
+- On peut voir une réduction du nombre d'opérations avec -02 et encore une réduction avec -03
+
+### Multi-tâche : Commandes utiles
+*Cette commande vous permet d’observer le statut des processus en cours d’exécution : Que signifient
+les lettres et symboles de la colonne STAT ? Etablissez une petite table contenant les symboles, l’état
+du processus et une brève description de leur sens.*
+
+![](/cours_REDS/PTR/PIERRICK/labo1_ptr/STATE_CODES.png)
+
+### Ordonnancement en temps partagé
+*Pouvez-vous expliquer le résultat de ces trois commandes ? Si vous avez des doutes, ajoutez getpid()
+à la sortie des printf de cpu_loop.c.*
+- NON
+
+*Si vous ouvrez 2 shells et utilisez la même commande taskset dans chacun, et exécutez ensuite la
+commande suivante sur le premier shell :*
+
+`$ sleep 1; ./cpu_loop & ./cpu_loop`
+
+*Et au même instant la commande suivante sur le second :*
+
+`$ ./cpu_loop & ./cpu_loop`
+
+*Quel résultat obtenez-vous ? Pourquoi ?*
+
+- OUI
+
 ### Migration de tâches
 *Exécutez ensuite, à l’aide de la commande taskset précédemment vue, le logiciel cpu_loop sur le  même  CPU  où get_cpu_number tourne.  Qu’est-ce  qu’il  se  passe ?*
 - Get_CPU se fait déloger de son emplacement.
 
 *Restreignez  l’exécution  de get_cpu_numbersur 2-3 CPUs, ouvrez un nombre correspondant de terminaux et essayez de déloger get_cpu_number de son CPU. Utilisez la commande htop pour visualiser la charge du système et commentez par écrit*
 - Get_cpu_number se fait déloger à chaque lancement de cpu_look sur un des CPUs. Dans htop, on peut voir la charge passer d'un processeur à l'autre lors du lancement d'un cpu_look.
+
+
+### Codage
+*Commentaires*
+
+*Quelles sont les différences entre niceness et priorités ?*
+La niceness est une priorité user-space alors que la priorité concerne le kernel linux.
--- ressources/t_priority.c
+++ ressources/t_priority.c
@@ -7,6 +7,7 @@
 
 #define EXECUTION_TIME    10/*In seconds*/
 
+
 /*Barrier variable*/
 pthread_barrier_t barr;
 
@@ -20,13 +21,27 @@ unsigned total_iterations;
 unsigned int *prio_value;
 long int *nb_iterations;
 
+pthread_t *threads;
+
+
+
+float testVal;
+
+void comptNbIter();
+
 void *f_thread(void *arg)
 {
-  /*...*/
+  //récupération ID et affichage
+  int id = *((int *) arg);
+  printf("%d\n",id);
+  //On attend tous les threads
+  pthread_barrier_wait(&barr);
+  comptNbIter(id);
+  return NULL;
 }
 int main(int argc,char **argv)
 {
-  /*...*/
+
 
   /*Parse input*/
   if(argc != 2)
@@ -41,8 +56,6 @@ int main(int argc,char **argv)
     return EXIT_FAILURE;
   }
 
-  /*...*/
-
   /*Get minimal and maximal priority values*/
   min_prio = sched_get_priority_min(SCHED_FIFO);
   max_prio = sched_get_priority_max(SCHED_FIFO);
@@ -51,25 +64,97 @@ int main(int argc,char **argv)
   /*Initialize barrier*/
   if(pthread_barrier_init(&barr, NULL, nb_threads))
   {
-
     fprintf(stderr, "Could not initialize barrier!\n");
     return EXIT_FAILURE;
   }
 
   /*Set priorities and create threads*/
-  fprintf(stdout,"%d\n",min_prio);
-    fprintf(stdout,"%d\n",max_prio);
+
+  /*Allocation des ressources*/
+  threads = malloc(nb_threads*sizeof(pthread_t));
+  prio_value = malloc(nb_threads*sizeof(unsigned int));
+  nb_iterations = malloc(nb_threads*sizeof(long int));
+
+  //Mise en place des prioritées
+  for(i=0; i < nb_threads;++i)
+  {
+    prio_value[i] = rand() % max_prio + 1;
+    pthread_setschedprio(threads[i], prio_value[i]);
+  }
+
+  //Création des threads
+  for(i=0;i < nb_threads;++i)
+  {
+    int *arg = malloc(sizeof(*arg));
+    *arg = i;
+    if(pthread_create(&threads[i], NULL, f_thread, arg) != 0)
+    {
+      perror("thread creation error");
+      return EXIT_FAILURE;
+    }
+  }
+
 
   /*Wait for the threads to complete and set the results*/
-
-  /*...*/
-
-/*  for(i = 0; i < nb_threads; ++i)
+  for(i = 0; i < nb_threads; i++)
   {
+    pthread_join(threads[i],NULL);
+  }
+
+  //Comptage nombre total d'itérations
+  for(i=0; i < nb_threads; i++)
+  {
+    total_iterations += nb_iterations[i];
+  }
+
+  //Affichage du résultat
+  for(i = 0; i < nb_threads; ++i)
+  {
+    fprintf(stdout,"%d\n",prio_value[i]);
     fprintf(stdout, "[%02d] %ld (%2.0f%%)\n",
     prio_value[i], nb_iterations[i],
     100.0*nb_iterations[i] / total_iterations);
   }
-*/
+
   return EXIT_SUCCESS;
+}
+
+
+void comptNbIter(int i)
+{
+  //initialisation
+  time_t start_time;
+  time_t temp_time;
+  unsigned int nb_iterations_thread;
+  unsigned int nb_iterations_sec;
+  unsigned int nb_sec_iter;
+
+  nb_sec_iter = 1;
+  nb_iterations_thread = 0;
+  nb_iterations_sec = 0;
+  start_time = time(NULL);
+  temp_time = start_time;
+
+  while(time(NULL) < (start_time + EXECUTION_TIME)) {
+    //Pour affichage toutes les secondes
+    if(temp_time + 1 <= time(NULL))
+    {
+      temp_time = time(NULL);
+      printf("nb iterations sec %d : 0x%x \n",nb_sec_iter++,nb_iterations_sec);
+      nb_iterations_thread += nb_iterations_sec;
+      nb_iterations_sec = 0;
+    }
+
+    ++nb_iterations_sec;
+  }
+
+  //On s'assure qu'on a bien ajouter le nombre d'iterations de la dernière seconde
+  if(nb_sec_iter == EXECUTION_TIME)
+  {
+    nb_iterations_sec = 0;
+  }
+  //Stockage nombre itérations
+  nb_iterations[i] = nb_iterations_thread;
+
+  printf("nb iterations tot (hex): 0x%x \n", nb_iterations_thread);
 }
